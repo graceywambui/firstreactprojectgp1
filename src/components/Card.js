33ca9e73ffe9f9b3f5d51188adcd911efa93acd6
grace njeri wambui <gracenjeriwambui00@gmail.com>
2023-05-15 19:14:26 +0300

    group 1 first react project

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+
+export function Card() {
+    return (
+      <div className="card">
+        <img src="https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"  alt="logo"></img> 
+        <h1>title</h1>
+        <h3>description</h3>
+      </div>
+    );
+  }
+  
---
+++ src/components/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Form() {
+    return (
+      <div className="form">
+        <form>
+            <input type='title'></input>
+            <br/>
+            <input type='description'></input>
+            <br/>
+           <button >Submit</button> 
+        </form>
+      </div>
+    );
+  }
+  
+  export default Form;
